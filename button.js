8d7212e9b6081c279ed9473c00b5f6d5bf60b911
MichelBDLC <michelbdlc@gmail.com>
2023-01-16 05:11:03 -0800

    test commit

---
+++ button.js
@@ -0,0 +1,9 @@
+const nav = document.querySelector("nav");
+
+//THIS IS THE FUNCTION THAT SHOWS NAV ON HOMEPAGE when button clicked
+function show() {
+    nav.style.display="list-item";
+}
+
+const searchbtn = document.querySelector('.search-btn');
+const searchInput = document.querySelector('search-input');
